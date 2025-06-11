5520ab4a11a37d865e43d3d473a0eaf800bf0217
Abylaikhan Chazhabay <abylaikhan.chazhabayev@freedompay.kz>
2025-06-11 17:22:29 +0500

    feat: created products page

---
+++ src/lib/components/+card.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	interface CardProps {
+		imgUrl: string;
+		title: string;
+		subtitle?: string;
+		description: string;
+	}
+
+	let { imgUrl, title, subtitle = '', description }: CardProps = $props();
+</script>
+
+<div class="card p-4 shadow-xl shadow-xl cursor-pointer">
+	<aside class="card__aside mb-3">
+		<figure class="card__figure">
+			<img src={imgUrl} alt="card-img" />
+		</figure>
+	</aside>
+	<header class="card__header mb-3">
+		<h2 class="card__title text-2xl">{title}</h2>
+		<h3 class="card__subtitle">{subtitle}</h3>
+	</header>
+	<div class="card__body">
+		<p class="card__description">
+			{description}
+		</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		max-width: 400px;
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { Product } from './types.ts';
+	import Card from '$lib/components/+card.svelte';
+
+	let productList = $state.raw<Product[]>([]);
+
+	let loading = $state<boolean>(false);
+
+	const getProducts = async () => {
+		try {
+			loading = true;
+			const response = await fetch(' https://api.escuelajs.co/api/v1/products');
+			productList = await response.json();
+		} catch (error) {
+			console.error('Error fetching products', error);
+		} finally {
+			loading = false;
+		}
+	};
+
+	onMount(getProducts);
+</script>
+
+<div class="p-4">
+	<h1 class="text-2xl m-4">Product List</h1>
+	<div class="grid grid-cols-4 gap-2 max-sm:grid-cols-1 max-sm:gap-2 max-md:grid-cols-3">
+		{#each productList as product}
+			<Card imgUrl={product.images[0]} title={product.title} description={product.description} />
+		{/each}
+	</div>
+</div>
